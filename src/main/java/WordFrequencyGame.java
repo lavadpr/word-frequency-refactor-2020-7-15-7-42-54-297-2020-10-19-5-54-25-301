20a890c3b33248933a11d7076a5e2f3a500f45ae
LAVADPR <lavadpr@oocl.com>
2020-10-19 15:35:35 +0800

    [word frequency] [prince] refactor - change s to words

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -12,10 +12,10 @@ public class WordFrequencyGame {
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = sentence.split(WHITESPACE);
+                String[] words = sentence.split(WHITESPACE);
 
                 List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
+                for (String s : words) {
                     Input input = new Input(s, 1);
                     inputList.add(input);
                 }
