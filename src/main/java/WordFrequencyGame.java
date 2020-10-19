4245fd0bf50cc2621d36acb93b06b0fe5cc66b8c
LAVADPR <lavadpr@oocl.com>
2020-10-19 16:08:46 +0800

    [word frequency] [prince] refactor - extract getwordinfolist method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -4,28 +4,12 @@ public class WordFrequencyGame {
 
     public static final String WHITESPACE = "\\s+";
 
-    public String getResult(String sentence){
-        if (sentence.split(WHITESPACE).length==1) {
+    public String getResult(String sentence) {
+        if (sentence.split(WHITESPACE).length == 1) {
             return sentence + " 1";
         } else {
             try {
-                String[] words = sentence.split(WHITESPACE);
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, 1);
-                    wordInfoList.add(wordInfo);
-                }
-                Map<String, List<WordInfo>> wordInfoMap = getWordInfoMap(wordInfoList);
-
-                List<WordInfo> wordInfos = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    wordInfos.add(wordInfo);
-                }
-                wordInfoList = wordInfos;
-
-                wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
+                List<WordInfo> wordInfoList = getWordInfoList(sentence);
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordInfo wordInfo : wordInfoList) {
@@ -39,24 +23,39 @@ public class WordFrequencyGame {
         }
     }
 
+    private List<WordInfo> getWordInfoList(String sentence) {
+        String[] words = sentence.split(WHITESPACE);
 
-    private Map<String,List<WordInfo>> getWordInfoMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> wordInfoMap = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
-            if (!wordInfoMap.containsKey(wordInfo.getValue())){
-                ArrayList wordInfos = new ArrayList<>();
-                wordInfos.add(wordInfo);
-                wordInfoMap.put(wordInfo.getValue(), wordInfos);
-            }
-
-            else {
-                wordInfoMap.get(wordInfo.getValue()).add(wordInfo);
-            }
+        List<WordInfo> wordInfoList = new ArrayList<>();
+        for (String word : words) {
+            WordInfo wordInfo = new WordInfo(word, 1);
+            wordInfoList.add(wordInfo);
         }
+        Map<String, List<WordInfo>> wordInfoMap = getWordInfoMap(wordInfoList);
 
+        List<WordInfo> wordInfos = new ArrayList<>();
+        for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
+            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+            wordInfos.add(wordInfo);
+        }
+        wordInfoList = wordInfos;
 
-        return wordInfoMap;
+        wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
+        return wordInfoList;
     }
 
 
+    private Map<String, List<WordInfo>> getWordInfoMap(List<WordInfo> wordInfoList) {
+        Map<String, List<WordInfo>> wordInfoMap = new HashMap<>();
+        for (WordInfo wordInfo : wordInfoList) {
+            if (!wordInfoMap.containsKey(wordInfo.getValue())) {
+                ArrayList wordInfos = new ArrayList<>();
+                wordInfos.add(wordInfo);
+                wordInfoMap.put(wordInfo.getValue(), wordInfos);
+            } else {
+                wordInfoMap.get(wordInfo.getValue()).add(wordInfo);
+            }
+        }
+        return wordInfoMap;
+    }
 }
