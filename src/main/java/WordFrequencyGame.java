841c8f1c6cfcb524221e2084f4d447bc3b08cb13
LAVADPR <lavadpr@oocl.com>
2020-10-19 15:38:43 +0800

    [word frequency] [prince] refactor - rename string s to word

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -15,8 +15,8 @@ public class WordFrequencyGame {
                 String[] words = sentence.split(WHITESPACE);
 
                 List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String s : words) {
-                    WordInfo wordInfo = new WordInfo(s, 1);
+                for (String word : words) {
+                    WordInfo wordInfo = new WordInfo(word, 1);
                     wordInfoList.add(wordInfo);
                 }
 
